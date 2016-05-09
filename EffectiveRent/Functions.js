68860cb28b8228bf6a6b92b3d05bc1949ec703ec
mmoroney <mikemoroney1722@hotmail.com>
2016-05-08 19:02:21 -0700

    Initial checkin

---
+++ EffectiveRent/Functions.js
@@ -0,0 +1,75 @@
+﻿/// <summary>
+/// Calculates the sum of a geometric series
+/// </summary>
+/// <param name="a">The first term in the series.</param>
+/// <param name="r">The common ratio between successive terms.</param>
+/// <param name="n">The number of terms in the series.</param>
+/// <returns></returns>
+function Sum(a, r, n)
+{
+    if (r == 1.0)
+        return a * n;
+
+    return a * (Math.pow(r, n) - 1) / (r - 1);
+}
+
+
+/// <summary>
+/// Calculates the payment for a mortgage.
+/// </summary>
+/// <param name="p">Initial principal of the loan.</param>
+/// <param name="i">The interest rate per period.</param>
+/// <param name="n">The number of periods.</param>
+/// <returns></returns>
+function Payment(p, i, n)
+{
+    return p * i / (1 - Math.pow(1 + i, -n));
+}
+
+/// <summary>
+/// Calculates the future value of an asset.
+/// </summary>
+/// <param name="a">Initial value of the asset.</param>
+/// <param name="i">The rate at which the asset increases per period.</param>
+/// <param name="n">The number of periods.</param>
+/// <returns></returns>
+function FutureValue(a, i, n)
+{
+    return a * Math.pow(1 + i, n);
+}
+
+/// <summary>
+/// Calculates the future value of a series of increasing payments
+/// </summary>
+/// <param name="a">Value of the initial payment.</param>
+/// <param name="i">Rate at which the amount of the payment increases per period.</param>
+/// <param name="r">Rate at which the value of all money paid increases per period.</param>
+/// <param name="n">The number of periods.</param>
+/// <returns></returns>
+function ProgressiveFutureValue(a, i, r, n)
+{
+    return Sum(a * Math.pow(1.0 + r, n - 1), (1.0 + i) / (1.0 + r), n);
+}
+
+/// <summary>
+/// Converts an annual interest rate to a monthly interest rate.
+/// </summary>
+/// <param name="annualRate">The annual interest rate.</param>
+/// <returns></returns>
+function ToMonthlyRate(annualRate)
+{
+    return Math.pow(annualRate + 1.0, 1.0 / 12) - 1.0;
+}
+
+/// <summary>
+/// Calculates the effective monthly expense based on a total opportunity cost.
+/// </summary>
+/// <param name="opportunityCost">The total opportunity cost.</param>
+/// <param name="inflationRate">The annual rate at which monthly expenses increase.</param>
+/// <param name="returnOnInvestments">The annual rate at which investments increase.</param>
+/// <param name="n">The number of payments to be made.</param>
+/// <returns></returns>
+function EffectiveMonthlyCost(opportunityCost, inflationRate, returnOnInvestments, n)
+{
+    return opportunityCost / (ProgressiveFutureValue(1.0 + inflationRate, inflationRate, returnOnInvestments, n));
+}
---
+++ EffectiveRent/HousePurchase.js
@@ -0,0 +1,45 @@
+﻿function DownpaymentOpportunityCost(downpayment, returnOnInvestments, timeHorizon) {
+    return FutureValue(downpayment, returnOnInvestments, timeHorizon);
+}
+
+function ClosingOpportunityCost(closingCosts, returnOnInvestments, timeHorizon) {
+    return FutureValue(closingCosts, returnOnInvestments, timeHorizon);
+}
+
+function MortagePaymentOpportunityCost(mortgagePayment, term, returnOnInvestments, timeHorizon) {
+    mortgagePaymentOpportunityCost = Sum(mortgagePayment, ToMonthlyRate(returnOnInvestments) + 1.0, 12 * Math.min(term, timeHorizon));
+    if (timeHorizon > term)
+        mortgagePaymentOpportunityCost = FutureValue(mortgagePaymentOpportunityCost, returnOnInvestments, timeHorizon - term);
+
+    return mortgagePaymentOpportunityCost;
+}
+
+function MonthlyExpensesOpportunityCost(monthlyExpenses, inflationRate, returnOnInvestments, timeHorizon) {
+    return ProgressiveFutureValue(monthlyExpenses, ToMonthlyRate(inflationRate), ToMonthlyRate(returnOnInvestments), timeHorizon * 12);
+}
+
+function PropertyTaxOpportunityCost(purchasePrice, propertyTaxRate, appreciationRate, returnOnInvestments, timeHorizon) {
+    return ProgressiveFutureValue(purchasePrice * propertyTaxRate * (1.0 + appreciationRate), appreciationRate, returnOnInvestments, timeHorizon);
+}
+
+function RemainingPrincipal(mortgagePayment, principal, interestRate, term, timeHorizon) {
+    if (timeHorizon >= term)
+        return 0;
+
+    return FutureValue(principal, ToMonthlyRate(interestRate), 12 * timeHorizon) - Sum(mortgagePayment, 1.0 + ToMonthlyRate(interestRate), 12 * timeHorizon);
+}
+
+function HouseValue(purchasePrice, appreciationRate, timeHorizon) {
+    return FutureValue(purchasePrice, appreciationRate, timeHorizon);
+}
+
+function TaxRefundValue(principal, mortgagePayment, interestRate, term, marginalTaxRate, returnOnInvestments, timeHorizon) {
+    n = Math.min(term, timeHorizon);
+
+    value = (Sum(12 * mortgagePayment, 1.0 + returnOnInvestments, n) + ProgressiveFutureValue((principal - mortgagePayment / ToMonthlyRate(interestRate)) * interestRate, interestRate, returnOnInvestments, n)) * marginalTaxRate;
+
+    if (timeHorizon > term)
+        value = FutureValue(value, returnOnInvestments, timeHorizon - term);
+
+    return value;
+}
